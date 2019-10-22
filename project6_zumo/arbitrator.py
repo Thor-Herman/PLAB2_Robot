534220504eebad7f8e442715e4301f5d0e19b94c
Hege <hegeaal@stud.ntnu.no>
2019-10-22 12:48:46 +0200

    added arbitrator

---
+++ project6_zumo/arbitrator.py
@@ -0,0 +1,12 @@
+class Arbitrator:
+
+    def choose_action(self, behaviour_list):
+        """Chooses which behaviour wins."""
+        heaviest_behaviour = ''
+        for behaviour in behaviour_list:
+            if behaviour.get_weight() > heaviest_behaviour.get_weight():
+                heaviest_behaviour = behaviour
+        best_choice = (heaviest_behaviour.get_motor_recommendations(), heaviest_behaviour.get_halt())
+        return best_choice
+
+
